34b4a0a47f294d671532f313e3b0935ada8355ed
Jeremy Vega <150966479+Kirigaya2002@users.noreply.github.com>
2025-05-22 20:00:09 -0600

    refactor(PromptServiceImpl): enhance full message construction for AI response
    Improve the full message construction to include specific instructions for the AI's behavior, ensuring concise and context-aware responses. This change aims to make the AI's responses more natural and aligned with user expectations.

--- src/infrastructure/services/PromptServiceImpl.js
+++ src/infrastructure/services/PromptServiceImpl.js
@@ -28,7 +28,11 @@ class PromptServiceImpl extends IPromptService {
             const contextResponse = await this.externalApiService.getContext(prompt);
 
             // Crear mensaje completo con el contexto
-            const fullMessage = `Mensaje del usuario: ${prompt}\nContexto encontrado en la db: ${contextResponse}\nRespuesta de la IA: `;
+            const fullMessage = `Mensaje del usuario: ${prompt}\nContexto encontrado en la db: ${contextResponse}\nRespuesta de la IA: 
+            Comportate como si ya supieras el contexto desde antes, y una persona te está preguntando a ti, actúa como un asistente, 
+            responde lo más corto pero conciso sobre lo que te pregunten, no te salgas del contexto ofrecido y responde solo lo solicitado, 
+            no tienes que ser seco, puedes actuar amable como si fueses un asistente real, puedes hablar entre mínimo 5 palabras y un máximo de 
+            500 palabras dependiendo el contexto y el mensaje recibido`;
 
             // Obtener respuesta final de la IA
             const finalResponse = await this.externalApiService.getAIResponse(fullMessage);
